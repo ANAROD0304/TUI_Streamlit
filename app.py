bfe2a6cfd99859041871a7f93eaf7be782867a74
ANAROD0304 <ANAGAROD@UCM.ES>
2025-08-19 21:23:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.title("🚀 Mi primera app en Streamlit")
 st.write("¡Hola! Esto está corriendo en Streamlit Cloud.")
+
+name = st.text_input("¿Cómo te llamas?")
+if name:
+    st.success(f"Encantado de conocerte, {name} 😃")
+
